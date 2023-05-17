fa3159a2a1ad95e7be9691b3c2df03aaa09508f3
vineboneto <vineboneto@gmail.com>
2023-05-16 21:03:40 -0300

    feat: atualizado dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,19 +4,13 @@ WORKDIR /usr/src/app
 
 COPY package.json yarn.lock ./
 
+RUN apt update && apt upgrade -y
+RUN apt install -y protobuf-compiler
+
 RUN yarn
+
 
 COPY . ./
 
-RUN yarn grpc_tools_node_protoc \
-  --plugin=protoc-gen-ts=./node_modules/.bin/grpc_tools_node_protoc_plugin \
-  --plugin=protoc-gen-grpc=./node_modules/.bin/protoc-gen-ts \
-  --js_out=import_style="commonjs,binary":./proto \
-  --ts_out=service=grpc-node:./proto \
-  --grpc_out=grpc_js:./proto \
-  --proto_path=./proto \
-  ./proto/*.proto && sed -i "s/require('grpc')/require('@grpc\/grpc-js')/g" ./proto/product_grpc_pb.js
-  
-
-
 EXPOSE 50051
+
--- compile_proto.sh
+++ compile_proto.sh
@@ -1,20 +1,21 @@
 # ! /bin/bash
-
 CURR_DIR=$(pwd)
-
 OUT_DIR="$CURR_DIR/proto"
 PROTO_DIR="$CURR_DIR/proto"
-
 PROTOC_GEN_GRPC_PATH="./node_modules/.bin/grpc_tools_node_protoc_plugin"
-
 PROTOC_GEN_TS_PATH="./node_modules/.bin/protoc-gen-ts"
-
-
 yarn grpc_tools_node_protoc \
+  --plugin=protoc-gen-grpc=$PROTOC_GEN_GRPC_PATH \
   --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH \
-  --plugin=protoc-gen-grpc=${PROTOC_GEN_GRPC_PATH} \
-  --js_out=import_style="commonjs,binary":$PROTO_DIR \
-  --ts_out=service=grpc-node:$OUT_DIR \
+  --js_out=import_style=commonjs,binary:$OUT_DIR \
   --grpc_out=grpc_js:$OUT_DIR \
-  --proto_path=$OUT_DIR \
-  $OUT_DIR/*.proto
+  --ts_out=grpc_js:$OUT_DIR \
+  --proto_path=$PROTO_DIR \
+  $PROTO_DIR/*.proto
+
+# generate d.ts codes
+protoc \
+    --plugin=protoc-gen-ts=./node_modules/.bin/protoc-gen-ts \
+    --ts_out=grpc_js:$OUT_DIR \
+    --proto_path=$PROTO_DIR \
+    $OUT_DIR/*.proto
